2077198a418be78101eb79e06e2d77896d6719f7
AndhikaPJ <andhika644@gmail.com>
2020-04-11 17:04:05 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Dosen.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Dosen {
+ public static void tampilanInfo () {
+	System.out.println();
+	System.out.println("==== Data Dosen ====");
+	System.out.println("NIDN : 2419069901 ");
+	System.out.println("Nama : Andika PJ ");
+	System.out.println("====================");
+ }
+}
---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa {
+	public static void tampilanInfo(){
+		System.out.println();
+		System.out.println("==== Data Mahasiswa ====");
+		System.out.println("NPM  : 18.63.0758 ");
+		System.out.println("Nama : Andhika PJ ");
+		System.out.println("========================");
+	}
+}
