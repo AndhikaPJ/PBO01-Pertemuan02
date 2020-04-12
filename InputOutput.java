84895398ce25a53e5c8273885b0ceed92790a5e9
Andhika Prima Jaya <andhika644@gmail.com>
2020-04-12 10:09:41 +0800

    Modif File Input dan Output
    Oke jeh

--- InputOutput.java
+++ InputOutput.java
@@ -4,12 +4,15 @@ public class InputOutput {
  public static void main (String [] args) {
 	Scanner scanner = new Scanner (System.in);
 	String nama;
+	String kelas;
 	int jumlahSKS;
 	double IPK;
 
 	System.out.print("Masukkan Nama Anda : ");
 	nama = scanner.nextLine();
-	System.out.println("Hai, "+nama);
+
+	System.out.print("Masukkan Kelas Anda : ");
+	kelas = scanner.nextLine();
 
 	System.out.print("Masukkan jumlah SKS yang sudah ditempuh : ");
 	jumlahSKS = scanner.nextInt();
@@ -18,6 +21,9 @@ public class InputOutput {
 	IPK = scanner.nextDouble();
 
 	double jumlah = IPK * jumlahSKS;
+
+	System.out.println("Hai, "+nama);
+	System.out.println("Kamu anak kelas, "+kelas);
 	System.out.print("Total Nilai = "+jumlah);
 	}
 }
